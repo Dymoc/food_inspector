f8d2e7f05f5ea9681563e8f9188f8062c734961a
Dymoc <dymoc@mail.ru>
2021-11-29 12:23:42 +0300

    Добавленна админка

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->enum('role', ['user', 'admin'])
+            $table->enum('user_role', ['user', 'admin'])
                 ->comment('Статус пользователя');
             $table->timestamps();
         });
