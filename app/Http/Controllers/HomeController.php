e4a9fc595eee17e830c061f700d7a595a1c5333b
RGK84 <rgk84@bk.ru>
2021-12-02 23:23:00 +0300

    Rand popular recipes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
                 'ingredientList' => Ingredient::all(),
                 'ingredientsCategories' => IngredientsCategory::all()
             ]);
-        
+
     }
 
 }
